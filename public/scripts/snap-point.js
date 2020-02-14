63547dc1f8a101ecec628c7ccf2ee6041462f343
Mitchell Koch <kochmitchell@outlook.com>
2020-02-14 09:59:11 -0500

    Actual cloning branch.

--- public/scripts/snap-point.js
+++ public/scripts/snap-point.js
@@ -68,39 +68,56 @@ AFRAME.registerComponent('snap-point', {
                 let targetSnapComp = targetEl.getAttribute('snap-point');
 
                 if (targetSnapComp) {
-                    if (targetSnapComp.snapId == data.snapTo) {
+                    if (targetSnapComp.isEnabled && targetSnapComp.snapId == data.snapTo) {
                         console.log("Snapping to " + data.snapTo);
 
                         data.isEnabled = false;
-                        //targetSnapComp.isEnabled = false;
+                        targetSnapComp.isEnabled = false;
                         console.log("Disabling snapping for " + this.data.snapId);
 
                         if (data.isParent)
                         {
-                            // Tried to create a clone a add it but, but it doesn't clone the bounding box.
-                            //let clone = targetEl.parentEl.cloneNode();
-                            //el.parentEl.appendChild(clone);
-                            //clone.setAttribute('position', { x: el.object3D.position.x, y: el.object3D.position.y, z: el.object3D.position.z})
-                            //clone.setAttribute('constraint', { target: "#" + el.parentEl.id, collideConnected: false });
-
-                            targetEl.parentEl.flushToDOM(true);
-
-                            el.parentEl.appendChild(targetEl.parentEl);
-                            targetEl.parentEl.setAttribute('rotation', { x: 0, y: 0, z: 0 })
-                            targetEl.parentEl.setAttribute('position', { x: el.object3D.position.x, y: el.object3D.position.y, z: el.object3D.position.z })
-                            targetEl.parentEl.setAttribute('constraint', { target: "#" + el.parentEl.id, collideConnected: false });
-
+                            // Tried to create a clone and add it but, but it doesn't clone the bounding box.
+                            //targetEl.parentEl.flushToDOM(true);
                             //targetEl.parentEl.flushToDOM();
-                            //targetEl.parentEl.setAttribute('static-body', '');
-                            //el.parentEl.appendChild(targetEl.parentEl);
 
+                            let clone = targetEl.parentEl.cloneNode();
+
+                            //clone.removeAttribute('body');
+                            //clone.removeAttribute('shape__main');
+
+                            clone.setAttribute('rotation', { x: 0, y: 0, z: 0 })
+                            clone.setAttribute('position', { x: el.object3D.position.x, y: -1 * targetEl.getAttribute('position').y, z: el.object3D.position.z });
+
+                            clone.setAttribute('dynamic-body', { mass: "1" });
+                            clone.setAttribute('constraint', { target: "#" + el.parentEl.id, collideConnected: false });
+
+                            clone.setAttribute('body', { type: 'dynamic', mass: "1", shape: 'none' });
+                            clone.setAttribute('shape__main', { shape: 'box', halfExtents: "1.3 0.09 0.9" });
+                            clone.removeAttribute('grabbable');
+
+                            el.parentEl.appendChild(clone);
+                            //targetEl.parentEl.removeAttribute('body')
+                            //targetEl.parentEl.removeAttribute('shape__main');
+                            //targetEl.parentEl.parentEl.removeChild(targetEl.parentEl);
+                            //clone.setAttribute('dynamic-body', { mass: "1" });
+                            //clone.removeAttribute('body');
+                            //clone.removeAttribute('shape__main');
+
+                            // Attaching the existing object. Sort of works but crash prone.
+                            //targetEl.parentEl.flushToDOM(true);
+
+                            //el.parentEl.appendChild(targetEl.parentEl);
+                            //targetEl.parentEl.setAttribute('rotation', { x: 0, y: 0, z: 0 })
+                            //targetEl.parentEl.setAttribute('position', { x: el.object3D.position.x, y: el.object3D.position.y, z: el.object3D.position.z })
+                            //targetEl.parentEl.setAttribute('constraint', { target: "#" + el.parentEl.id, collideConnected: false });
                         }
                         else
                         {
-                            el.parentEl.setAttribute('constraint', { target: "#" + targetEl.parentEl.id, collideConnected: false });
+                            //el.parentEl.setAttribute('constraint', { target: "#" + targetEl.parentEl.id, collideConnected: false });
                         }
 
-                        el.parentEl.removeChild(el);
+                        //el.sceneEl.removeChild(el);
                         el.removeEventListener("collisions", this.eventHandlerFn);
                     }
                 }
