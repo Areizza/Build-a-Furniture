81e16e3f3938769f13e4d3759e45578a0c51ca2e
Maxime Vincent <34112203+MaxDaMiner@users.noreply.github.com>
2020-03-24 21:07:21 -0400

    Resized living room and components for consistency

--- public/scripts/systems/builder.js
+++ public/scripts/systems/builder.js
@@ -33,12 +33,12 @@ AFRAME.registerSystem('builder', {
             console.log("Spawning piece");
             let spawner = document.createElement("a-entity");
             spawner.setAttribute('class', 'clickable');
-            spawner.setAttribute('geometry', { primitive: 'box', width: 1, height: 1, depth: 1});
+            spawner.setAttribute('geometry', { primitive: 'box', width: 0.3, height: 0.3, depth: 0.3});
             spawner.setAttribute('material', { color: '#996600', });
             spawner.setAttribute('dynamic-body', '');
             spawner.setAttribute('hoverable', '');
             spawner.setAttribute('spawner', { furniture: 'table', piece: data.pieceId, isOneUse: true });
-            spawner.setAttribute('position', { x: -7, y: 1, z: 1});
+            spawner.setAttribute('position', { x: -2.3, y: 1, z: 0.3});
 
             sceneEl.appendChild(spawner);
 
