086de27b003dae9081e4669256ef9ac8ff4c8d7e
Mitchell Koch <kochmitchell@outlook.com>
2020-03-31 17:07:09 -0400

    Updated the shelfRack model, and removed the preloaded models.

--- public/scripts/templates.js
+++ public/scripts/templates.js
@@ -1,5 +1,6 @@
 const templates =
 {
+    // TABLE
     "table":
         [
             {
@@ -8,7 +9,7 @@ const templates =
                 "furniture": "tier: 0; totalSnapPoints: 4;",
                 "mixin": "part",
                 "scale": "1 1 1",
-                "gltf": "#tableTopModel",
+                "gltf": "/assets/models/table/tableTop/tableTop.gltf",
                 "body": "type: dynamic; mass: 20; shape: none;",
                 "shape__main": "shape: box; halfExtents: 0.72 0.0225 0.72",
 
@@ -72,7 +73,7 @@ const templates =
                 "mixin": "part",
                 "furniture": "tier: 1; totalSnapPoints: 1;",
                 "scale": "1 1 1",
-                "gltf": "#tableLegModel",
+                "gltf": "/assets/models/table/tableLeg/tableLeg.gltf",
                 "body": "type: dynamic; mass: 10; shape: none;",
                 "shape__main": "shape: box; halfExtents: 0.036 0.345 0.036;",
 
@@ -93,7 +94,7 @@ const templates =
                 "mixin": "part",
                 "furniture": "tier: 1; totalSnapPoints: 1;",
                 "scale": "1 1 1",
-                "gltf": "#tableSkirtModel",
+                "gltf": "/assets/models/table/tableSkirt/tableSkirt.gltf",
                 "body": "type: dynamic; mass: 10; shape: none;",
                 "shape__main": "shape: box; halfExtents: 0.6 0.0395 0.02;",
 
@@ -109,6 +110,7 @@ const templates =
             }
         ],
 
+    // SHELF
     "shelf":
         [
             {
@@ -117,7 +119,7 @@ const templates =
                 "furniture": "tier: 0; totalSnapPoints: 9;",
                 "mixin": "part",
                 "scale": "0.125 0.125 0.125",
-                "gltf": "#shelfBackModel",
+                "gltf": "assets/models/shelf/shelfBack/shelfBack.gltf",
                 "body": "type: dynamic; mass: 20; shape: none;",
                 "shape__main": "shape: box; halfExtents: 3.95 8.65 0.08",
 
@@ -181,7 +183,7 @@ const templates =
                 "mixin": "part",
                 "furniture": "tier: 1; totalSnapPoints: 1;",
                 "scale": "0.125 0.125 0.125",
-                "gltf": "#shelfRackModel",
+                "gltf": "assets/models/shelf/shelfRack/shelfRack.gltf",
                 "body": "type: dynamic; mass: 6; shape: none;",
                 "shape__main": "shape: box; halfExtents: 3.94 0.088 1.54;",
 
@@ -202,7 +204,7 @@ const templates =
                 "mixin": "part",
                 "furniture": "tier: 1; totalSnapPoints: 1;",
                 "scale": "0.125 0.125 0.125",
-                "gltf": "#shelfSideModel",
+                "gltf": "/assets/models/shelf/shelfSideBoard/shelfSide.gltf",
                 "body": "type: dynamic; mass: 10; shape: none;",
                 "shape__main": "shape: box; halfExtents: 0.281 9.7 1.8;",
 
@@ -223,7 +225,7 @@ const templates =
                 "mixin": "part",
                 "furniture": "tier: 2; totalSnapPoints: 1;",
                 "scale": "0.125 0.125 0.125",
-                "gltf": "#shelfSkirtModel",
+                "gltf": "/assets/models/shelf/shelfSkirt/shelfSkirt.gltf",
                 "body": "type: dynamic; mass: 6; shape: none;",
                 "shape__main": "shape: box; halfExtents: 3.97 0.275 0.098;",
 
@@ -244,7 +246,7 @@ const templates =
                 "mixin": "part",
                 "furniture": "tier: 1; totalSnapPoints: 2;",
                 "scale": "0.125 0.125 0.125",
-                "gltf": "#shelfTopModel",
+                "gltf": "/assets/models/shelf/shelfTop/shelfTop.gltf",
                 "body": "type: dynamic; mass: 10; shape: none;",
                 "shape__main": "shape: box; halfExtents: 4.5 0.15 1.85;",
 
@@ -264,10 +266,162 @@ const templates =
                 "class": "grabbable shelf shelfBottom",
                 "mixin": "part",
                 "furniture": "tier: 1; totalSnapPoints: 1;",
-                "scale": "0.13 0.13 0.13",
-                "gltf": "#shelfBottomModel",
+                "scale": "0.25 0.25 0.25",
+                "gltf": "assets/models/shelf/shelfBottom/shelfBottom.gltf",
                 "body": "type: dynamic; mass: 10; shape: none;",
                 "shape__main": "shape: box; halfExtents: 3.965 0.28 1.79;",
+
+                "entity":
+                    [
+                        {
+                            "position": "0 0.345 0",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 2; snapTo: 1;"
+                        }
+                    ]
+            },
+        ],
+
+    // CHAIR
+    "chair":
+        [
+            {
+                "id": "chairBack",
+                "class": "grabbable chair chairBack",
+                "furniture": "tier: 0; totalSnapPoints: 9;",
+                "mixin": "part",
+                "scale": "1 1 1",
+                "gltf": "/assets/models/chair/chairBack/chairBack.gltf",
+                "body": "type: dynamic; mass: 12; shape: none;",
+                "shape__main": "shape: box; halfExtents: 3.95 8.65 0.08",
+
+                "entity":
+                    [
+                        {
+                            "position": "0 -0.045 0.62",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 3; snapTo: 4;"
+                        },
+                        {
+                            "position": "-0 -0.045 -0.62",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 3; snapTo: 4;"
+                        },
+                        {
+                            "position": "-0.62 -0.045 0",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 3; snapTo: 4;"
+                        },
+                        {
+                            "position": "0.62 -0.045 0",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 3; snapTo: 4;"
+                        },
+
+                        {
+                            "position": "0.62 -0.045 0.62",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 1; snapTo: 2;"
+                        },
+                        {
+                            "position": "-0.62 -0.045 -0.62",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 1; snapTo: 2;"
+                        },
+                        {
+                            "position": "-0.62 -0.045 0.62",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 1; snapTo: 2;"
+                        },
+                        {
+                            "position": "0.62 -0.045 -0.62",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 1; snapTo: 2;"
+                        }
+                    ]
+            },
+
+            {
+                "id": "chairBackLeg",
+                "class": "grabbable chair chairBackLeg",
+                "mixin": "part",
+                "furniture": "tier: 1; totalSnapPoints: 1;",
+                "scale": "1 1 1",
+                "gltf": "assets/models/chair/chairLegBack/chairLegBack.gltf",
+                "body": "type: dynamic; mass: 10; shape: none;",
+                "shape__main": "shape: box; halfExtents: 3.94 0.088 1.54;",
+
+                "entity":
+                    [
+                        {
+                            "position": "0 0.345 0",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 2; snapTo: 1;"
+                        }
+                    ]
+            },
+
+            {
+                "id": "chairFrontLeg",
+                "class": "grabbable chair chairFrontLeg",
+                "mixin": "part",
+                "furniture": "tier: 1; totalSnapPoints: 1;",
+                "scale": "1 1 1",
+                "gltf": "/assets/models/chair/chairLegFront/chairLegFront.gltf",
+                "body": "type: dynamic; mass: 10; shape: none;",
+                "shape__main": "shape: box; halfExtents: 0.281 9.7 1.8;",
+
+                "entity":
+                    [
+                        {
+                            "position": "0 0.345 0",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 2; snapTo: 1;"
+                        }
+                    ]
+            },
+
+            {
+                "id": "chairSeat",
+                "class": "grabbable chair chairSeat",
+                "mixin": "part",
+                "furniture": "tier: 2; totalSnapPoints: 1;",
+                "scale": "1 1 1",
+                "gltf": "/assets/models/chair/chairSeat/chairSeat.gltf",
+                "body": "type: dynamic; mass: 6; shape: none;",
+                "shape__main": "shape: box; halfExtents: 3.97 0.275 0.098;",
+
+                "entity":
+                    [
+                        {
+                            "position": "0 0.09 0",
+                            "class": "snapPoint",
+                            "mixin": "sphereCollider",
+                            "snap_point": "snapId: 4; snapTo: 3;"
+                        }
+                    ]
+            },
+
+            {
+                "id": "chairSkirt",
+                "class": "grabbable chair chairSkirt",
+                "mixin": "part",
+                "furniture": "tier: 1; totalSnapPoints: 2;",
+                "scale": "1 1 1",
+                "gltf": "/assets/models/chair/chairSkirt/chairSkirt.gltf",
+                "body": "type: dynamic; mass: 6; shape: none;",
+                "shape__main": "shape: box; halfExtents: 4.5 0.15 1.85;",
 
                 "entity":
                     [
